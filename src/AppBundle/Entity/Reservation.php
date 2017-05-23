6e4b8b7cc0f6c31875d1d453777e212cf3714b06
Kevin SI <sikevin.sk@gmail.com>
2017-05-23 16:15:04 +0200

    création de l'entité visitor

--- src/AppBundle/Entity/Reservation.php
+++ src/AppBundle/Entity/Reservation.php
@@ -17,112 +17,29 @@ class Reservation
      */
     private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255, name="lastname")
-     */
-    private $lastname;
-
-    /**
-     * @ORM\Column(type="string", length=255, name="firstname")
-     */
-    private $firstname;
-
-    /**
-     * @ORM\Column(type="date")
-     */
+	/**
+	 * @ORM\Column(type="date")
+	 */
     private $reservationDate;
 
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $ticketType;
-
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $tariff;
-
-    /**
-     * @ORM\Column(type="date")
-     */
-    private $birthdate;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $country;
-
 	/**
-	 * @ORM\Column(type="string", length=255, name="reservation_code")
+	 * @ORM\Column(type="boolean")
 	 */
-	private $reservationCode;
+    private $ticketType;
 
 	/**
 	 * @ORM\Column(type="string", length=255)
 	 */
-	private $email;
+    private $reservationCode;
 
-	public function __construct()
-	{
-		$this->reservationDate = new \Datetime();
-	}
-
-	/**
+    /**
      * Get id
      *
-     * @return int
+     * @return integer
      */
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * Set lastname
-     *
-     * @param string $lastname
-     *
-     * @return Reservation
-     */
-    public function setLastname($lastname)
-    {
-        $this->lastname = $lastname;
-
-        return $this;
-    }
-
-    /**
-     * Get lastname
-     *
-     * @return string
-     */
-    public function getLastname()
-    {
-        return $this->lastname;
-    }
-
-    /**
-     * Set firstname
-     *
-     * @param string $firstname
-     *
-     * @return Reservation
-     */
-    public function setFirstname($firstname)
-    {
-        $this->firstname= $firstname;
-
-        return $this;
-    }
-
-    /**
-     * Get firstname
-     *
-     * @return string
-     */
-    public function getFirstname()
-    {
-        return $this->firstname;
     }
 
     /**
@@ -166,109 +83,12 @@ class Reservation
     /**
      * Get ticketType
      *
-     * @return bool
+     * @return boolean
      */
     public function getTicketType()
     {
         return $this->ticketType;
     }
-
-    /**
-     * Set tariff
-     *
-     * @param boolean $tariff
-     *
-     * @return Reservation
-     */
-    public function setTariff($tariff)
-    {
-    	// True = Journée & False = Demi-journée
-        $this->tariff = $tariff;
-
-        return $this;
-    }
-
-    /**
-     * Get tariff
-     *
-     * @return bool
-     */
-    public function getTariff()
-    {
-        return $this->tariff;
-    }
-
-    /**
-     * Set birthdate
-     *
-     * @param \DateTime $birthdate
-     *
-     * @return Reservation
-     */
-    public function setBirthdate($birthdate)
-    {
-        $this->birthdate = $birthdate;
-
-        return $this;
-    }
-
-    /**
-     * Get birthdate
-     *
-     * @return \DateTime
-     */
-    public function getBirthdate()
-    {
-        return $this->birthdate;
-    }
-
-    /**
-     * Set country
-     *
-     * @param string $country
-     *
-     * @return Reservation
-     */
-    public function setCountry($country)
-    {
-        $this->country = $country;
-
-        return $this;
-    }
-
-    /**
-     * Get country
-     *
-     * @return string
-     */
-    public function getCountry()
-    {
-        return $this->country;
-    }
-
-	/**
-	 * Set reservationCode
-	 *
-	 * @param string $reservationCode
-	 *
-	 * @return Reservation
-	 */
-	public function setResevationCode($reservationCode)
-	{
-		$this->reservationCode = $reservationCode;
-
-		return $this;
-	}
-
-	/**
-	 * Get reservationCode
-	 *
-	 * @return string
-	 */
-	public function getReservationCode()
-	{
-		return $this->reservationCode;
-	}
 
     /**
      * Set reservationCode
@@ -285,26 +105,12 @@ class Reservation
     }
 
     /**
-     * Set email
-     *
-     * @param string $email
-     *
-     * @return Reservation
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get email
+     * Get reservationCode
      *
      * @return string
      */
-    public function getEmail()
+    public function getReservationCode()
     {
-        return $this->email;
+        return $this->reservationCode;
     }
 }
---
+++ src/AppBundle/Entity/Visitor.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Visitor
+ *
+ * @ORM\Table(name="visitor")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\VisitorRepository")
+ */
+class Visitor
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Reservation")
+	 * @ORM\JoinColumn(nullable=false)
+     */
+    private $reservation;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastname", type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthdate", type="date")
+     */
+    private $birthdate;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="tariff", type="boolean")
+     */
+    private $tariff;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set reservation
+     *
+     * @param string $reservation
+     *
+     * @return Visitor
+     */
+    public function setReservation($reservation)
+    {
+        $this->reservation = $reservation;
+
+        return $this;
+    }
+
+    /**
+     * Get reservation
+     *
+     * @return string
+     */
+    public function getReservation()
+    {
+        return $this->reservation;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return Visitor
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return Visitor
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return Visitor
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set birthdate
+     *
+     * @param \DateTime $birthdate
+     *
+     * @return Visitor
+     */
+    public function setBirthdate($birthdate)
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    /**
+     * Get birthdate
+     *
+     * @return \DateTime
+     */
+    public function getBirthdate()
+    {
+        return $this->birthdate;
+    }
+
+    /**
+     * Set tariff
+     *
+     * @param boolean $tariff
+     *
+     * @return Visitor
+     */
+    public function setTariff($tariff)
+    {
+        $this->tariff = $tariff;
+
+        return $this;
+    }
+
+    /**
+     * Get tariff
+     *
+     * @return bool
+     */
+    public function getTariff()
+    {
+        return $this->tariff;
+    }
+}
+
