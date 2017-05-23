5ba69ae0837a0c16a7b1626fcfb47dcfd5a1c130
Kevin SI <sikevin.sk@gmail.com>
2017-05-23 13:22:12 +0200

    Modifications sur la vue et le controller et ajout d'une entité reservation

--- src/AppBundle/Controller/ReservationController.php
+++ src/AppBundle/Controller/ReservationController.php
@@ -12,6 +12,10 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Reservation;
+
+use AppBundle\Form\ReservationType;
+
 
 class ReservationController extends Controller
 {
@@ -20,6 +24,48 @@ class ReservationController extends Controller
 	 */
 	public function showAction()
 	{
-		return $this->render('reservation/form1.html.twig');
+		$reservationId = 1;
+		$reservation1 = $this->getDoctrine()
+			->getRepository('AppBundle:Reservation')
+			->find($reservationId);
+
+		if (!$reservation1)
+		{
+			throw $this->createNotFoundException(
+				'No product found for id '. $reservationId);
+		}
+
+
+		$reservation = new Reservation();
+		$form = $this->createForm(ReservationType::class, $reservation);
+
+		return $this->render('reservation/form1.html.twig',[
+			'reservation' => $reservation1,
+			'form' 		  => $form
+		]);
+	}
+
+	/**
+	 * @Route("/add/")
+	 */
+	public function createAction()
+	{
+		// TEST réussi: add a reservation to DB
+		$reservation = new Reservation();
+		$reservation->setLastname("SI");
+		$reservation->setFirstname("Kevin");
+		$reservation->setBirthdate(new \DateTime("1995-06-20"));
+		$reservation->setCountry("France");
+		$reservation->setReservationDate(new \DateTime('NOW'));
+		$reservation->setTariff(false);
+		$reservation->setTicketType(true);
+
+		$em = $this->getDoctrine()->getManager();
+
+		$em->persist($reservation);
+
+		$em->flush();
+
+		return new Response('Saved new reservation with id' . $reservation->getId());
 	}
 }
---
+++ src/AppBundle/Form/ReservationType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+			->add('lastname',			TextType::class)
+			->add('firstname',			TextType::class)
+			->add('reservationDate',	DateTimeType::class)
+			->add('ticketType',		RadioType::class)
+			->add('tariff',			RadioType::class)
+			->add('birthdate',			DateTimeType::class)
+			->add('country',			TextType::class)
+			->add('reservationCode',	TextType::class)
+			->add('email',				TextType::class)
+		;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Reservation'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_reservation';
+    }
+
+
+}
